2af300041a3f158cfb6bcb90a8dd138d10e159b5
Saptak <saptakbarsul123@gmail.com>
2025-11-21 20:42:39 +0530

    8

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -318,12 +318,33 @@ export const getRecommendationsForMood = async (
 export const getCurrentUserProfile = (accessToken: string) =>
   fetchSpotify<UserProfile>(`${SPOTIFY_API_BASE}/me`, accessToken);
 
+// Hardcoded list of valid Spotify genre seeds (as of Nov 2024)
+// The /recommendations/available-genre-seeds endpoint has been deprecated
+const SPOTIFY_GENRE_SEEDS = [
+  "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal",
+  "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop",
+  "chicago-house", "children", "chill", "classical", "club", "comedy", "country",
+  "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco",
+  "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic",
+  "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth",
+  "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore",
+  "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house",
+  "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance",
+  "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino",
+  "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno",
+  "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party",
+  "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop",
+  "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day",
+  "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly",
+  "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter",
+  "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study",
+  "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop",
+  "turkish", "work-out", "world-music"
+];
+
 export const getAvailableGenreSeeds = async (accessToken: string): Promise<string[]> => {
-  const data = await fetchSpotify<{ genres: string[] }>(
-    `${SPOTIFY_API_BASE}/recommendations/available-genre-seeds`,
-    accessToken
-  );
-  return data.genres;
+  // Return the hardcoded list since the API endpoint has been deprecated
+  return SPOTIFY_GENRE_SEEDS;
 };
 
 export const createPlaylistWithTracks = async (
